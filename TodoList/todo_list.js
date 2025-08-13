76db45ced276c601ef787db790577227f1f9e51c
tejeshwarsiddarth <tejeshwarsiddarths@gmail.com>
2025-08-13 09:28:16 -0400

    clear all button added

--- TodoList/todo_list.js
+++ TodoList/todo_list.js
@@ -2,6 +2,7 @@ const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 const clearCompletedBtn = document.getElementById("clearCompletedBtn");
+const clearAllBtn = document.getElementById("clearAllBtn");
 
 let tasks = [];
 
@@ -32,6 +33,11 @@ function displayTasks() {
             tasks = tasks.filter(task => !task.completed);
             displayTasks();
         }
+        function clearAll(){
+            tasks = [];
+            displayTasks();
+        }
         addTaskBtn.addEventListener("click", addTask);
 clearCompletedBtn.addEventListener("click", clearCompletedTasks);
+clearAllBtn.addEventListener("click", clearAll);
  displayTasks();
